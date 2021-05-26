135d91b0404d161e1941c086b596fb909b5047b6
yaojie <limyaojie93@gmail.com>
2021-05-26 20:58:23 +0800

    Add sample styling

--- assets/js/pages/products/backlog/BacklogCard.tsx
+++ assets/js/pages/products/backlog/BacklogCard.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BacklogItem } from "../types";
 
 const component: React.FC<{ item: BacklogItem }> = ({ item }) => {
-  return (<div key={item.id}>{item.title}</div>)
+  const [title, setTitle] = useState<string>(item.title);
+
+  return (<div className="p-5 pl-1 bg-blue-200 mb-2" key={item.id}>{title}</div>)
 };
 
 export default component;
