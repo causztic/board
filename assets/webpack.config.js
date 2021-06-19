4cbc0d544ba49fd072501ed9bb6d74fa872345a8
yaojie <limyaojie93@gmail.com>
2021-06-19 14:59:20 +0800

    Upgrade webpack to 5

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,8 +1,7 @@
 const path = require('path');
 const glob = require('glob');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => {
@@ -12,7 +11,7 @@ module.exports = (env, options) => {
     optimization: {
       minimizer: [
         new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode }),
-        new OptimizeCSSAssetsPlugin({})
+        new CssMinimizerPlugin(),
       ]
     },
     entry: {
@@ -37,7 +36,9 @@ module.exports = (env, options) => {
       ]
     },
     plugins: [
-      new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
+      new CopyWebpackPlugin({
+        patterns: [{ from: 'static/', to: '../' }]
+      })
     ],
     resolve: {
       extensions: ['.ts', '.tsx', '.js', '.jsx']
